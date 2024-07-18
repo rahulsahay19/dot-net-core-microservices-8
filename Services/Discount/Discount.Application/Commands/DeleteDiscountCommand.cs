67e89f667fc152fbbdc0d0d5bf935dbdf74f4da4
rahulsahay19 <rahulsahay19@hotmail.com>
2024-07-18 21:53:06 +0530

    initial commit

---
+++ Services/Discount/Discount.Application/Commands/DeleteDiscountCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Discount.Application.Commands
+{
+    public class DeleteDiscountCommand : IRequest<bool>
+    {
+        public string ProductName { get; set; }
+        public DeleteDiscountCommand(string productName)
+        {
+            ProductName = productName;
+        }
+    }
+}
---
+++ Services/Discount/Discount.Core/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Core.Entities;
+
+namespace Discount.Core.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
