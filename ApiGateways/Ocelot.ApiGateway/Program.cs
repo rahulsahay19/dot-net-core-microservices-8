89b60dfdd7e69438f0b5f787ab773414d29bff36
rahulsahay19 <rahulsahay19@hotmail.com>
2024-08-15 12:38:12 +0530

    initial ocelot setup

---
+++ ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -0,0 +1,38 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//ocelot configuration
+builder.Host.ConfigureAppConfiguration((env, config) =>
+{
+    config.AddJsonFile($"ocelot.{env.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello Ocelot"); });
+});
+
+await app.UseOcelot();
+await app.RunAsync();
